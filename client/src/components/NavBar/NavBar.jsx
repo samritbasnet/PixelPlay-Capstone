0b3bdd380fa785733edafbfb46f2f41df140d624
Samrit Basnet <samritbasnet70@gmail.com>
2025-03-29 23:38:40 -0400

    navbar final changes

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,26 +1,58 @@
-import { Link } from "react-router-dom";
-import pixelplay from "../../assets/logo/logo.png";
+import { useState } from "react";
+import {
+  FaGamepad,
+  FaHeart,
+  FaSearch,
+  FaUserShield
+} from "react-icons/fa";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import "./NavBar.scss";
 
 function NavBar() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (query.trim()) {
+      navigate(`/search?q=${encodeURIComponent(query.trim())}`);
+      setQuery("");
+    }
+  };
+
   return (
     <nav className="navbar">
-      <div className="navbar__container">
-        
-        <Link to="/" className="navbar__brand">
-          <img src={pixelplay} alt="PixelPlay Logo" className="navbar__logo" />
-          PixelPlay
-        </Link>
+      <Link to="/" className="navbar__brand">
+        <FaGamepad className="navbar__logo" />
+        PixelPlay
+      </Link>
 
-        
-        <ul className="navbar__links">
-          <li>
-            <Link to="/pixelshelf" className="navbar__link">PixelShelf</Link>
-          </li>
-          <li>
-            <Link to="/admin" className="navbar__link">Admin</Link>
-          </li>
-        </ul>
+      <div className="navbar__links-container">
+    
+      <form className="navbar__search-form" onSubmit={handleSearch}>
+          <input
+            type="text"
+            placeholder="Search for games..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            className="navbar__search-input"
+          />
+          <button type="submit" className="navbar__search-button">
+            <FaSearch />
+          </button>
+        </form>
+
+        <NavLink to="/wishlist" className="navbar__link">
+          <FaHeart className="navbar__icon" />
+          PixelShelf
+        </NavLink>
+
+        <NavLink to="/admin" className="navbar__link">
+          <FaUserShield className="navbar__icon" />
+          User
+        </NavLink>
+
+     
       </div>
     </nav>
   );
